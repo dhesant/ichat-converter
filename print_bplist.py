96b7f28af533464c37cf0792e601e75e505f5a71
Dhesant Nakka <dhesant@gmail.com>
2017-12-19 20:13:13 +0800

    updated print_bplist to dump as CSV

--- print_bplist.py
+++ print_bplist.py
@@ -1,7 +1,7 @@
 from ccl_bplist import ccl_bplist
 import sys
 import os
-import pprint
+#import pprint
 
 if (len(sys.argv) < 2):
     print("No argument. Usage: \"print_plist [FILE]\"")
@@ -9,14 +9,23 @@ if (len(sys.argv) < 2):
 
 fn = sys.argv[1]
 
-if os.path.isfile(fn):
-    f = open(fn, "rb")
-    plist = ccl_bplist.load(f)
-    obj = ccl_bplist.deserialise_NsKeyedArchiver(plist)
-    pp=pprint.PrettyPrinter(indent=1)
-    pp.pprint(obj)
-
-else:
+if not os.path.isfile(fn):
     print("Invalid file")
+    sys.exit(1)
+
+f = open(fn, "rb")
+plist = ccl_bplist.load(f)
+obj = ccl_bplist.deserialise_NsKeyedArchiver(plist)
+
+lst = obj['NS.objects'][2]['NS.objects']
+
+print("Time,Sender,Message(,BColor, Font, FColor, FSize")
+
+for msg in lst:
+    if msg['Sender'] == '$null':
+        print(msg['Time']['NS.time'], ",", msg['OriginalMessage'], " , System")
+    else:
+        print(msg['Time']['NS.time'], ",", msg['OriginalMessage'], ",", msg['Sender']['ID'], ",", msg['Color']['NSRGB'], ",", msg['MessageText']['NSAttributes']['NS.objects'][2]['NSName'], ",", msg['MessageText']['NSAttributes']['NS.objects'][0]['NSRGB'], ",", msg['MessageText']['NSAttributes']['NS.objects'][2]['NSSize'])
+
 
 
