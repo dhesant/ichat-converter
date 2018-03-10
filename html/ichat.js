7caa810ff1633d14851e1b30ddfbff49a2f61f6a
Dhesant Nakka <dhesant@gmail.com>
2018-03-10 14:43:22 +0800

    added time tooltip; chat with banner

--- html/ichat.js
+++ html/ichat.js
@@ -11,27 +11,35 @@ $(document).on('change', '#customFile', function(event) {
 
 function parse_ichat(jsonObj) {
     // Clear containers
+    $("#info").text("");
     $("#viewer").empty();
 
     // Init System
     console.log('Protocol: ' + jsonObj['NS.objects'][0]);
-    var sysUser = jsonObj['NS.objects'][3][0]['ServiceLoginID']
+    var sysUser = jsonObj['NS.objects'][3][0]['ServiceLoginID'];
+    var numUsers = jsonObj['NS.objects'][3].length;
 
-    // Get CSS class mappings per user
+    // Map color classes & get chat name
     var color = {};
+    var status = "Chat with "
     color[sysUser] = "host-message";
     _.each(jsonObj['NS.objects'][3], function(usr, index) {
 	color[usr['ID']] = "guest-message color-" + (index % 15);
+	status = status + usr['ID'];
+	if (index != (numUsers - 1)) {
+	    status = status + ', ';
+	}
     });
+
+    $("#info").text(status);
 
     // Group messages by user
     gmsgs = group_message(jsonObj['NS.objects'][2]);
-    console.log(gmsgs);
-    
+        
     // Create message blocks for each message group
     _.each(gmsgs, function(msgs, index) {
 	var container = $('<div id="message-block-' + index + '" class="message-block d-flex flex-column"></div>');
-	if (msgs[0]['Sender'] !== null && msgs[0]['Sender']['ID'] != msgs[0]['Sender']['ServiceLoginID']) {
+	if (numUsers > 2 && msgs[0]['Sender'] !== null && msgs[0]['Sender']['ID'] != msgs[0]['Sender']['ServiceLoginID']) {
 	    $("#viewer").append('<span class="username">' + msgs[0]['Sender']['ID'] + '</span>');
 	}
 
@@ -49,7 +57,7 @@ function parse_ichat(jsonObj) {
 		    container = $('<span class="system-message user-message">' + usrmsg.split('/me').join('<b>' + msg['Sender']['ID'] + '</b>') + '</span>')
 		}
 		else {
-		    var elem = $('<span class="message">' + usrmsg + '</span>')
+		    var elem = $('<span class="message" data-toggle="tooltip" data-placement="top" title="' + msg['Time'] + '">' + usrmsg + '</span>')
 		    elem.addClass(color[msg['Sender']['ID']])
 		}
 	    }
